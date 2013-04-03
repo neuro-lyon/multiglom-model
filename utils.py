41c0b002983fcc910e6a5d3070d5510de2b7cc45
Vincent <vincent@ra.fake>
2013-04-03 11:15:53 +0200

    Adds a new method to set the global model params.

--- utils.py
+++ utils.py
@@ -3,9 +3,17 @@ Utilities not related to neuronal network simulation.
 
 """
 
+import importlib
+import model
+
 def path_to_modline(filepath):
     """Returns a string representation for module import of a file."""
     # Import the set of parameters
     pathsplit = filepath.split('/')
     modname = pathsplit[-1][:-3]
     return '.'.join(pathsplit[:-1] + [modname])
+
+def set_model_ps(filepath, dicname='PARAMETERS'):
+    """Set the model parameters given the parameter set in `filepath`."""
+    psmod = importlib.import_module(path_to_modline(filepath))
+    model.PARAMETERS = getattr(psmod, dicname)
