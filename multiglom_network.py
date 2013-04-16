8b4345d88cd7588852ee410232ce24f74ec20861
Vincent <vincent@ra.fake>
2013-04-16 16:45:26 +0200

    Set a main() function in multiglom script
    This is done so we can easily run the script from another file using an
    import.

--- multiglom_network.py
+++ multiglom_network.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python2.7
 # -*- coding:utf-8 -*-
-
 """
 Multi-glomerular Network Simulation
 ===================================
@@ -27,225 +26,229 @@ Script Overview
 8. Plots
 
 """
-
-# Argument parsing
-import argparse
-
-APARSER = argparse.ArgumentParser(description="Run a multi-glomerular simulation.")
-APARSER.add_argument('psfile')
-APARSER.add_argument('--no-plot', action='store_true')
-APARSER.add_argument('--no-indexes', action='store_true')
-APARSER.add_argument('--full-ps', action='store_true')
-ARGS = APARSER.parse_args()
-
-# Set the parameters from the specified file BEFORE any model.* import
 from brian import *
-import model_utils as mutils
-import model
-mutils.set_model_ps(ARGS.psfile)
-
-import numpy as np
-from scipy.fftpack import fft, fftfreq
-import analysis
-import plotting
-from utils import print_dict
-
-from model.glomerule import Glomerule
-from model.mitral_cells import MitralCells
-from model.synapse import Synapse
-from model.granule_cells import GranuleCells
-
-# Reset old stuff from Brian memory
-clear(erase=True, all=True)
-defaultclock.reinit()
 
 
-"""
-Parameters
-----------
-Get the parameter values from the `ps` module, which in turn gets the values
-from the file specified in parameters.py.
+def main(args):
+    import model_utils as mutils
+    # Set the parameters from the specified file BEFORE any model.* import
+    import model
+    mutils.set_model_ps(args.psfile)
 
-Set some aliases for the different cell population sizes.
-Also check that there is an even number of cells for each column.
+    import numpy as np
+    from scipy.fftpack import fft, fftfreq
+    import analysis
+    import plotting
+    from utils import print_dict
 
-Finally set some simulation parameters.
-
-"""
-PSGM     = model.PARAMETERS['Glomerule']
-PSMT     = model.PARAMETERS['Mitral']
-PSGR     = model.PARAMETERS['Granule']
-PSCOMMON = model.PARAMETERS['Common']
-
-N_MITRAL    = PSCOMMON['N_mitral']
-N_GLOMERULI = N_GRANULE = N_SUBPOP = PSCOMMON['N_subpop']
-
-# check to have an even number of mitral in each sub-population
-assert N_MITRAL % N_SUBPOP == 0, \
-       "N_mitral is not a multiple of the number of sub-populations N_subpop."
-N_MITRAL_PER_SUBPOP = N_MITRAL/N_SUBPOP
-
-defaultclock.dt = PSCOMMON['simu_dt']
-SIMU_LENGTH     = PSCOMMON['simu_length']
+    from model.glomerule import Glomerule
+    from model.mitral_cells import MitralCells
+    from model.synapse import Synapse
+    from model.granule_cells import GranuleCells
+    # Reset old stuff from Brian memory
+    clear(erase=True, all=True)
+    defaultclock.reinit()
 
 
-"""
-Population Initialization
--------------------------
-1. glomeruli
-*. synapses between granule and mitral cells
-3. mitral cells
-4. granule cells
+    """
+    Parameters
+    ----------
+    Get the parameter values from the `ps` module, which in turn gets the values
+    from the file specified in parameters.py.
 
-"""
-# Glomeruli
-glom = Glomerule()
-glom.add_eqs(oscillating=False)
-glom.make_pop(N_GLOMERULI*N_MITRAL_PER_SUBPOP)
+    Set some aliases for the different cell population sizes.
+    Also check that there is an even number of cells for each column.
 
-# Synapses (granule -- mitral)
-synexc = Synapse(synapse_type='exc') # excitatory synapse
-synexc.set_eqs_model()
+    Finally set some simulation parameters.
 
-syninhib = Synapse(synapse_type='inhib') # inhibitory synapse
-syninhib.set_eqs_model()
+    """
+    PSGM     = model.PARAMETERS['Glomerule']
+    psmt     = model.PARAMETERS['Mitral']
+    psgr     = model.PARAMETERS['Granule']
+    pscommon = model.PARAMETERS['Common']
 
-# Mitral cells
-mt = MitralCells()
-mt_supp_eqs =  {'var': ['- I_syn', '- g_input*V'],
-                'eqs': [synexc.get_eqs_model(),
-                        Equations("g_input : siemens*meter**-2")]}
-mt.add_eqs(supp_eqs=mt_supp_eqs)
-mt.make_pop(N_MITRAL)
-mt.pop.V = (PSMT['E_L'] - PSMT['V_r'])*np.random.random_sample(np.shape(mt.pop.V)) \
-           + PSMT['V_r']
+    n_mitral    = pscommon['N_mitral']
+    n_glomeruli = n_granule = n_subpop = pscommon['N_subpop']
 
-# Granule Cells
-gr = GranuleCells()
-gr_supp_eqs = {'var': ['-I_syn'],
-               'eqs': [syninhib.get_eqs_model()]}
-gr.add_eqs(supp_eqs=gr_supp_eqs)
-gr.make_pop(N_GRANULE)
-gr.pop.V_D = PSGR['E_L']
-gr.pop.V_S = PSGR['E_L']
+    # check to have an even number of mitral in each sub-population
+    assert n_mitral % n_subpop == 0, \
+           "N_mitral is not a multiple of the number of sub-populations N_subpop."
+    n_mitral_per_subpop = n_mitral/n_subpop
+
+    defaultclock.dt = pscommon['simu_dt']
+    simu_length     = pscommon['simu_length']
 
 
-"""
-Connecting Populations
-----------------------
-1. Glomeruli and mitral cells 
-2. Mitral cells and granule cells
+    """
+    Population Initialization
+    -------------------------
+    1. glomeruli
+    *. synapses between granule and mitral cells
+    3. mitral cells
+    4. granule cells
 
-"""
-# Connecting mitral cells to glomeruli
-glmt_connections = diag(ones(N_MITRAL))
+    """
+    # Glomeruli
+    glom = Glomerule()
+    glom.add_eqs(oscillating=False)
+    glom.make_pop(n_glomeruli*n_mitral_per_subpop)
 
-# Glomeruli--Mitral interactions
-@network_operation(when='start')
-def mt_input():
-    mt.pop.g_input = dot(glom.pop.g, glmt_connections)
+    # Synapses (granule -- mitral)
+    synexc = Synapse(synapse_type='exc') # excitatory synapse
+    synexc.set_eqs_model()
 
-# Connecting sub-population of mitral cells to granule cells
-mtgr_connections = mutils.intrapop_connections(N_MITRAL, N_GRANULE, N_SUBPOP, N_MITRAL_PER_SUBPOP)
+    syninhib = Synapse(synapse_type='inhib') # inhibitory synapse
+    syninhib.set_eqs_model()
 
-# Inter subpopulation connectivities
-INTER_CONN_RATE = PSCOMMON['inter_conn_rate']
-INTER_CONN_STRENGTH = PSCOMMON['inter_conn_strength']
-mtgr_connections = mutils.interpop_connections(mtgr_connections, N_MITRAL, N_SUBPOP,
-                                        N_MITRAL_PER_SUBPOP, INTER_CONN_RATE, INTER_CONN_STRENGTH)
-# Mitral--Granule interactions
-@network_operation(when='start')
-def graded_synapse():
-    """Computes granule and mitral s_syn"""
-    mt.pop.state('T')[:] = 0.
-    mt.pop.state('T')[mt.pop.get_refractory_indices()] = 1.
-    gr.pop.s_syn = dot(mt.pop.s, mtgr_connections)
-    mt.pop.s_syn = dot(gr.pop.s, transpose(mtgr_connections))
+    # Mitral cells
+    mt = MitralCells()
+    mt_supp_eqs =  {'var': ['- I_syn', '- g_input*V'],
+                    'eqs': [synexc.get_eqs_model(),
+                            Equations("g_input : siemens*meter**-2")]}
+    mt.add_eqs(supp_eqs=mt_supp_eqs)
+    mt.make_pop(n_mitral)
+    mt.pop.V = (psmt['E_L'] - psmt['V_r'])*np.random.random_sample(np.shape(mt.pop.V)) \
+               + psmt['V_r']
 
-@network_operation(when='start')
-def sum_s():
-    """Computes granule self s_syn (for its glomerular column only)"""
-    for subpop in xrange(N_SUBPOP):
-        start = subpop*N_MITRAL_PER_SUBPOP
-        stop  = start + N_MITRAL_PER_SUBPOP
-        gr.pop.s_syn_self[subpop] = sum(mt.pop.state('s')[start:stop])
-
-@network_operation(when='after_groups')
-def keep_reset():
-    mt.pop.state('V')[mt.pop.get_refractory_indices()] = PSMT['V_r']
+    # Granule Cells
+    gr = GranuleCells()
+    gr_supp_eqs = {'var': ['-I_syn'],
+                   'eqs': [syninhib.get_eqs_model()]}
+    gr.add_eqs(supp_eqs=gr_supp_eqs)
+    gr.make_pop(n_granule)
+    gr.pop.V_D = psgr['E_L']
+    gr.pop.V_S = psgr['E_L']
 
 
-"""
-Simulation Monitoring
----------------------
-Monitor state variables for the different populations.
+    """
+    Connecting Populations
+    ----------------------
+    1. Glomeruli and mitral cells 
+    2. Mitral cells and granule cells
 
-"""
-rec_neurons = [0, N_MITRAL/2, N_MITRAL - 1]
-glom_ps = ('g')
-mt_ps   = ('s', 's_syn', 'V')
-gr_ps   = ('V_D', 's_syn', 's', 's_syn_self')
+    """
+    # Connecting mitral cells to glomeruli
+    glmt_connections = diag(ones(n_mitral))
 
-# Simulation monitors
-monit_glom = mutils.monit(glom.pop, glom_ps, rec_neurons)
-monit_mt   = mutils.monit(mt.pop, mt_ps, rec_neurons, spikes=True)
-monit_gr   = mutils.monit(gr.pop, gr_ps)
+    # Glomeruli--Mitral interactions
+    @network_operation(when='start')
+    def mt_input():
+        mt.pop.g_input = dot(glom.pop.g, glmt_connections)
 
+    # Connecting sub-population of mitral cells to granule cells
+    mtgr_connections = mutils.intrapop_connections(n_mitral, n_granule, n_subpop, n_mitral_per_subpop)
 
-"""
-Running Simulation
-------------------
-Create Network object and put everything simulation related in it.
-Then run this network.
+    # Inter subpopulation connectivities
+    inter_conn_rate = pscommon['inter_conn_rate']
+    inter_conn_strength = pscommon['inter_conn_strength']
+    mtgr_connections = mutils.interpop_connections(mtgr_connections, n_mitral, n_subpop,
+                                            n_mitral_per_subpop, inter_conn_rate, inter_conn_strength)
+    # Mitral--Granule interactions
+    @network_operation(when='start')
+    def graded_synapse():
+        """Computes granule and mitral s_syn"""
+        mt.pop.state('T')[:] = 0.
+        mt.pop.state('T')[mt.pop.get_refractory_indices()] = 1.
+        gr.pop.s_syn = dot(mt.pop.s, mtgr_connections)
+        mt.pop.s_syn = dot(gr.pop.s, transpose(mtgr_connections))
 
-"""
-# Gathering simulation objects
-netw = Network(glom.pop, mt.pop, gr.pop,
-               mt_input, graded_synapse, keep_reset, sum_s,
-               [m for m in monit_glom.values()],
-               [m for m in monit_mt.values()],
-               [m for m in monit_gr.values()])
+    @network_operation(when='start')
+    def sum_s():
+        """Computes granule self s_syn (for its glomerular column only)"""
+        for subpop in xrange(n_subpop):
+            start = subpop*n_mitral_per_subpop
+            stop  = start + n_mitral_per_subpop
+            gr.pop.s_syn_self[subpop] = sum(mt.pop.state('s')[start:stop])
 
-# Simulation run
-netw.run(SIMU_LENGTH, report="text")
-
-
-"""
-Information Output
-------------------
-
-"""
-print '\nParameters: using', ARGS.psfile
-
-print 'Populations:', N_SUBPOP, 'glomerular columns;',
-print N_MITRAL, 'mitral cells;', N_GRANULE, 'granule cells.'
-
-print 'Times:', SIMU_LENGTH, 'of simulation; dt =', defaultclock.dt, '.'
-
-if ARGS.full_ps:
-    print 'Full set of parameters:'
-    print_dict(model.PARAMETERS)
-
-if not ARGS.no_indexes:
-    sts_index = analysis.sts(monit_gr['s_syn'], monit_mt['spikes'])
-    mps_index = analysis.mps(monit_mt['V'])
-    print 'Indexes: STS =', sts_index, '; MPS =', mps_index, '.'
+    @network_operation(when='after_groups')
+    def keep_reset():
+        mt.pop.state('V')[mt.pop.get_refractory_indices()] = psmt['V_r']
 
 
-"""
-Plotting
---------
-Plot monitored variables and a scatter plot.
+    """
+    Simulation Monitoring
+    ---------------------
+    Monitor state variables for the different populations.
 
-"""
-if not ARGS.no_plot:
-    # Raster plot
-    plotting.raster_plot(monit_mt['spikes'], N_SUBPOP)
+    """
+    rec_neurons = [0, n_mitral/2, n_mitral - 1]
+    glom_ps = ('g')
+    mt_ps   = ('s', 's_syn', 'V')
+    gr_ps   = ('V_D', 's_syn', 's', 's_syn_self')
 
-    # Membrane potentials
-    plotting.memb_plot_figure(monit_mt, monit_gr, rec_neurons, N_GRANULE)
+    # Simulation monitors
+    monit_glom = mutils.monit(glom.pop, glom_ps, rec_neurons)
+    monit_mt   = mutils.monit(mt.pop, mt_ps, rec_neurons, spikes=True)
+    monit_gr   = mutils.monit(gr.pop, gr_ps)
 
-    # Granule synapses
-    plotting.granule_figure(monit_gr, PSCOMMON)
 
-    show()
+    """
+    Running Simulation
+    ------------------
+    Create Network object and put everything simulation related in it.
+    Then run this network.
+
+    """
+    # Gathering simulation objects
+    netw = Network(glom.pop, mt.pop, gr.pop,
+                   mt_input, graded_synapse, keep_reset, sum_s,
+                   [m for m in monit_glom.values()],
+                   [m for m in monit_mt.values()],
+                   [m for m in monit_gr.values()])
+
+    # Simulation run
+    netw.run(simu_length, report="text")
+
+
+    """
+    Information Output
+    ------------------
+
+    """
+    print '\nParameters: using', args.psfile
+
+    print 'Populations:', n_subpop, 'glomerular columns;',
+    print n_mitral, 'mitral cells;', n_granule, 'granule cells.'
+
+    print 'Times:', simu_length, 'of simulation; dt =', defaultclock.dt, '.'
+
+    if args.full_ps:
+        print 'Full set of parameters:'
+        print_dict(model.PARAMETERS)
+
+    if not args.no_indexes:
+        sts_index = analysis.sts(monit_gr['s_syn'], monit_mt['spikes'])
+        mps_index = analysis.mps(monit_mt['V'])
+        print 'Indexes: STS =', sts_index, '; MPS =', mps_index, '.'
+
+
+    """
+    Plotting
+    --------
+    Plot monitored variables and a scatter plot.
+
+    """
+    if not args.no_plot:
+        # Raster plot
+        plotting.raster_plot(monit_mt['spikes'], n_subpop)
+
+        # Membrane potentials
+        plotting.memb_plot_figure(monit_mt, monit_gr, rec_neurons, n_granule)
+
+        # Granule synapses
+        plotting.granule_figure(monit_gr, pscommon)
+
+        show()
+
+
+if __name__ == '__main__':
+    # Argument parsing
+    import argparse
+    aparser = argparse.ArgumentParser(description="Run a multi-glomerular simulation.")
+    aparser.add_argument('psfile')
+    aparser.add_argument('--no-plot', action='store_true')
+    aparser.add_argument('--no-indexes', action='store_true')
+    aparser.add_argument('--full-ps', action='store_true')
+
+    args = aparser.parse_args()
+    main(args)
