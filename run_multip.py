e1bfa65952160c0e4a86e036f22f275a71fcb74d
Vincent <vincent@ra.fake>
2013-04-16 16:58:58 +0200

    No more file creation in multi run script
    This is a draft for the new HDF5 pseudo-DB implementation.

--- run_multip.py
+++ run_multip.py
@@ -3,30 +3,38 @@
 Script for running multiple simulation with different parameter set.
 
 """
-from subprocess import check_output
 from multiprocessing import Pool
-from os import listdir, mkdir, path
+from os import listdir, path
 from sys import argv
 
 
-def run_simu((psfile, dir_runs)):
-    """Runs the main script with specified parameter file."""
-    ndir = psfile[:-3]
-    mkdir(ndir)
-    simu_output = check_output(["python2.7", "multiglom_network.py", psfile,
-                                "--full-ps"])
-    with open(path.join(ndir, "output.txt"), 'w') as f:
-        f.write(simu_output)
+def new_simu(psfile):
+    # Creating simulation run arguments
+    from arg_parser import APARSER
+    args = APARSER.parse_args()
+    args.no_plot = True
+
+
+def get_pset_files(dir_paramsets):
+    """Return a list of parameter set files"""
+    psfiles = listdir(dir_paramsets)
+
+    def pset_filter(fname):
+        return fname[-3:] == '.py' and fname != '__init__.py'
+    psfiles = filter(pset_filter, psfiles)
+
+    psfiles = [path.join(dir_paramsets, fname) for fname in psfiles]
+    return psfiles
 
 
 if __name__ == '__main__':
+    # Get run arguments
     n_processes = int(argv[1])
-    dir_runs = argv[2]
+    dir_paramsets = argv[2]
 
-    psfiles = listdir(dir_runs)
-    psfile_filter = lambda fname: fname[-3:] == '.py' and fname != '__init__.py'
-    psfiles = filter(psfile_filter, psfiles)
-    psfiles = [(path.join(dir_runs, fname), dir_runs) for fname in psfiles]
+    # Get all the parameter set files
+    psfiles = get_pset_files(dir_paramsets)
 
+    # Run the simulation with each parameter set
     pool = Pool(processes=n_processes)
     pool.map(run_simu, psfiles)
