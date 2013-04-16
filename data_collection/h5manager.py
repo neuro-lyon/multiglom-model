b75f82e7d0a1b2da63ed6dce0f22f058a6d8db0a
Vincent <vincent@ra.fake>
2013-04-16 14:07:35 +0200

    Better checking for file existence

--- data_collection/h5manager.py
+++ data_collection/h5manager.py
@@ -1,22 +1,26 @@
 # -*- coding:utf-8 -*-
 
 import tables
-import numpy as np
-
-
-N_SIMU = 'n_simu'
+import os
 
 
 def init_data_h5(filename):
     """Initialize a data HDF5 file"""
-    with tables.openFile(filename, 'w') as f:
-        f.createArray('/', N_SIMU, np.array(0))
+    # Raise en error if the file already exists
+    try:
+        os.open(filename, os.O_WRONLY | os.O_CREAT | os.O_EXCL, 0644)
+    except OSError, e:
+        raise e
+    # Else, continue by creating the file
+    else:
+        with tables.openFile(filename, 'w') as f:
+            setattr(f.root._v_attrs, 'n_simu', 0)
 
 
 def new_simu(filename, data):
     """Put the simulation data into the HDF5 file"""
     with tables.openFile(filename, 'a') as f:
-        n_simu = f.getNode('/', N_SIMU).read()
+        n_simu = getattr(f.root._v_attrs, 'n_simu')
         # parse data and put them in a new group
         simu_group = f.createGroup('/', 'simu' + str(n_simu))
         # TODO change value of n_simu
