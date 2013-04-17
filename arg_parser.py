f60967c883b1364347c00e3ec6b7756db7428388
Vincent <vincent@ra.fake>
2013-04-17 10:44:53 +0200

    Add an argument parser for multi simulations

--- arg_parser.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf-8 -*-
-import argparse
-
-APARSER = argparse.ArgumentParser(description="Run a multi-glomerular simulation.")
-APARSER.add_argument('psfile')
-APARSER.add_argument('--no-plot', action='store_true')
-APARSER.add_argument('--no-indexes', action='store_true')
-APARSER.add_argument('--full-ps', action='store_true')
