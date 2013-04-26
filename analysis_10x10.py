2817fb02788782431391bae446d1ac7d40f6a6cb
Vincent <vincent@ra.fake>
2013-04-26 15:02:46 +0200

    Add STS figure to analysis_10x10 script

--- analysis_10x10.py
+++ analysis_10x10.py
@@ -6,47 +6,53 @@ import tables
 from h5manager import get_all_attrs
 from utils import to1d
 
-"""
-TODO
-- Faire tous les MPS
-- Faire tous les STS
-
-"""
 
 plt.ion()
 DB = tables.openFile('db.h5')
 
 
 """
-MPS
+MPS & STS
 
 """
-MPS_FIG, MPS_AXS = plt.subplots(3, 1)
-
-MPS_ATTRS = (('paramset', '_v_attrs', 'Common', 'inter_conn_rate', 0, 1),
+IDX_ATTRS = (('paramset', '_v_attrs', 'Common', 'inter_conn_rate', 0, 1),
              ('paramset', '_v_attrs', 'Common', 'inter_conn_strength', 0, 1),
-             ('results', '_v_attrs', 'MPS'))
-MPS = get_all_attrs(DB, MPS_ATTRS)
-MPS.sort()
-X_MPS = list(set([i[0] for i in MPS]))
-X_MPS.sort()
-Y_MPS = list(set([i[1] for i in MPS]))
-Y_MPS.sort()
+             ('results', '_v_attrs', 'MPS'),
+             ('results', '_v_attrs', 'STS'))
+IDX = get_all_attrs(DB, IDX_ATTRS)
+IDX.sort()
+X_IDX = list(set([i[0] for i in IDX]))
+X_IDX.sort()
+Y_IDX = list(set([i[1] for i in IDX]))
+Y_IDX.sort()
 
-Z_MPS = []
-for i in xrange(3):
-    Z_MPS.append(np.zeros((len(X_MPS), len(Y_MPS))))
-for ind_rate in xrange(len(X_MPS)):
-    for ind_strength in xrange(len(Y_MPS)):
-        tmp_mps = MPS[to1d(ind_rate, ind_strength, len(X_MPS))][2]
-        Z_MPS[0][ind_rate][ind_strength] = tmp_mps[0]
-        Z_MPS[1][ind_rate][ind_strength] = tmp_mps[1]
-        Z_MPS[2][ind_rate][ind_strength] = tmp_mps['whole']
+Z_IDX = []
+for i in xrange(6):
+    Z_IDX.append(np.zeros((len(X_IDX), len(Y_IDX))))
+for ind_rate in xrange(len(X_IDX)):
+    for ind_strength in xrange(len(Y_IDX)):
+        tmp_mps = IDX[to1d(ind_rate, ind_strength, len(X_IDX))][2]
+        Z_IDX[0][ind_rate][ind_strength] = tmp_mps[0]
+        Z_IDX[1][ind_rate][ind_strength] = tmp_mps[1]
+        Z_IDX[2][ind_rate][ind_strength] = tmp_mps['whole']
 
+        tmp_sts = IDX[to1d(ind_rate, ind_strength, len(X_IDX))][3]
+        Z_IDX[3][ind_rate][ind_strength] = tmp_sts[0]
+        Z_IDX[4][ind_rate][ind_strength] = tmp_sts[1]
+        Z_IDX[5][ind_rate][ind_strength] = tmp_sts['whole']
+
+# MPS plotting
+IDX_FIG, IDX_AXS = plt.subplots(3)
 for i in xrange(3):
-    cs = MPS_AXS[i].contourf(X_MPS, Y_MPS, Z_MPS[i], MPS_AXS[i],
-                            levels=np.linspace(0, 1, 11))
-    MPS_FIG.colorbar(cs, ax=MPS_AXS[i])
+    cs = IDX_AXS[i].contourf(X_IDX, Y_IDX, Z_IDX[i])
+    IDX_FIG.colorbar(cs, ax=IDX_AXS[i])
+plt.show()
+
+# STS plotting
+IDX_FIG, IDX_AXS = plt.subplots(3)
+for i in xrange(3):
+    cs = IDX_AXS[i].contourf(X_IDX, Y_IDX, Z_IDX[i + 3])
+    IDX_FIG.colorbar(cs, ax=IDX_AXS[i])
 plt.show()
 
 
@@ -54,7 +60,6 @@ plt.show()
 DEPHASAGE
 
 """
-
 PHI_ATTRS = (('paramset', '_v_attrs', 'Common', 'inter_conn_rate', 0, 1),
              ('paramset', '_v_attrs', 'Common', 'inter_conn_strength', 0, 1),
              ('results', '_v_attrs', 'phase_angles', 0, 1))
@@ -73,6 +78,8 @@ for ind_rate in xrange(len(X_PHI)):
 PHI_FIG = plt.figure()
 PHI_CS = plt.contourf(X_PHI, Y_PHI, Z_PHI)
 PHI_FIG.colorbar(PHI_CS)
+plt.ioff()
 plt.show()
+
 
 DB.close()
