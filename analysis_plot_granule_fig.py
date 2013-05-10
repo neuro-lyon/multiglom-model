a1772bfcc48c2fe361053d81483ee7f31d330f45
Vincent <vincent@ra.fake>
2013-05-10 18:32:00 +0200

    Add better simulation selection
    We can now select a simulation from indices in the (interco_rate,
    interco_strength) axes.
    
    Also fix the new path to db.

--- analysis_plot_granule_fig.py
+++ analysis_plot_granule_fig.py
@@ -13,7 +13,7 @@ import h5manager as h5m
 from plotting import granule_pop_figure
 from analysis import fftmax
 
-DB_FILENAME = "db_two_glom_beta_new_ps_interco_strength0_1_interco_rate0_1.h5"
+DB_FILENAME = "data/db30x30_two_glom_beta_new_ps_interco_strength0_1_interco_rate0_1.h5"
 DB = tables.openFile(DB_FILENAME)
 
 # Get all simulation data
@@ -25,6 +25,12 @@ ATTRS = (('paramset', '_v_attrs', 'Common', 'inter_conn_rate', 0, 1),
          ('results', 's_syn_self'),
          ('results', '_v_attrs'))
 ALL_SIMU_ATTRS = h5m.get_all_attrs(DB, ATTRS)
+
+SET_INTERCO_RATE = list(set(s[0] for s in ALL_SIMU_ATTRS))
+SET_INTERCO_RATE.sort()
+SET_INTERCO_STRENGTH = list(set(s[1] for s in ALL_SIMU_ATTRS))
+SET_INTERCO_STRENGTH.sort()
+
 
 # Some useful functions
 def deq(a, b, delta):
@@ -40,7 +46,9 @@ class DummyPkg:
 def get_interco(simu, interco_rate, interco_strength):
     return deq(simu[0], interco_rate, 0.02) and deq(simu[1], interco_strength, 0.02)
 
-GOOD_SIMUS = filter(lambda x : get_interco(x, 0.793, 0.918), ALL_SIMU_ATTRS)
+INTERCO_RATE = SET_INTERCO_RATE[25]
+INTERCO_STRENGTH = SET_INTERCO_STRENGTH[4]
+GOOD_SIMUS = filter(lambda x : get_interco(x, INTERCO_RATE, INTERCO_STRENGTH), ALL_SIMU_ATTRS)
 
 REDO_FFTMAX = []
 for simu in GOOD_SIMUS:
