1e1cd2dcbacf8b1a01d15171a933ea9b0ed51ea1
Vincent <vincent@ra.fake>
2013-05-27 15:24:41 +0200

    Fix bugs that selected multiple simu instead of 1

--- analysis_compare_individual_simulations.py
+++ analysis_compare_individual_simulations.py
@@ -8,12 +8,13 @@ scatter plot to compare the simulations.
 
 import matplotlib.pyplot as plt
 import tables
+from numpy import allclose
 
 import h5manager as h5m
 from plotting import granule_pop_figure
 from analysis import fftmax
 
-DB_FILENAME = "data/db30x30_two_glom_beta_new_ps_interco_strength0_1_interco_rate0_1.h5"
+DB_FILENAME = "data/db30x30_beta_homeostasis.h5"
 DB = tables.openFile(DB_FILENAME)
 
 # Get all simulation data
@@ -27,6 +28,7 @@ ATTRS = (('paramset', '_v_attrs', 'Common', 'inter_conn_rate', 0, 1),
          ('results', 'spikes_it'),
 )
 ALL_SIMU_ATTRS = h5m.get_all_attrs(DB, ATTRS)
+ALL_SIMU_ATTRS.sort()
 
 SET_INTERCO_RATE = list(set(s[0] for s in ALL_SIMU_ATTRS))
 SET_INTERCO_RATE.sort()
@@ -48,29 +50,34 @@ class SignalRepack:
 def get_interco(simu, interco_rate, interco_strength):
     return deq(simu[0], interco_rate, 0.02) and deq(simu[1], interco_strength, 0.02)
 
-SELECTED_RATES = [28]
-SELECTED_STRENGTH = [0, 10, 20, 25]
+SELECTED_RATES = [20, 25]
+SELECTED_STRENGTH = [6]
+REDO_FFTMAX = []
 for rate in SELECTED_RATES:
     for strength in SELECTED_STRENGTH:
-        INTERCO_RATE = SET_INTERCO_RATE[rate]
-        INTERCO_STRENGTH = SET_INTERCO_STRENGTH[strength]
-        GOOD_SIMUS = filter(lambda x: get_interco(x, INTERCO_RATE, INTERCO_STRENGTH), ALL_SIMU_ATTRS)
+        interco_rate = SET_INTERCO_RATE[rate]
+        interco_strength = SET_INTERCO_STRENGTH[strength]
+        filtered_simu = filter(lambda x: allclose(x[0], interco_rate) \
+                                         and allclose(x[1], interco_strength),
+                               ALL_SIMU_ATTRS)
+        if len(filtered_simu) > 1:
+            print "Warning: more than one simulation satisfy your criteria,",
+            print "selecting the first one."
+        simu = filtered_simu[0]
 
-        REDO_FFTMAX = []
-        for simu in GOOD_SIMUS:
-            gr_s = simu[4].read()
-            gr_s_syn_self = simu[5].read()
-            times = simu[2].read()
-            dt = float(simu[3])
-            granule_pop_figure(gr_s, gr_s_syn_self, times, dt)
+        gr_s = simu[4].read()
+        gr_s_syn_self = simu[5].read()
+        times = simu[2].read()
+        dt = float(simu[3])
+        granule_pop_figure(gr_s, gr_s_syn_self, times, dt)
 
-            signal = SignalRepack(gr_s, times)
-            REDO_FFTMAX.append(fftmax(signal, 2, dt))
+        signal = SignalRepack(gr_s, times)
+        REDO_FFTMAX.append(fftmax(signal, 2, dt))
 
-            spikes_it = simu[7].read()
-            plt.figure()
-            plt.title("rate: "+str(rate)+", strength: "+str(strength))
-            plt.plot(spikes_it[1], spikes_it[0], ' .', mew=0)
+        spikes_it = simu[7].read()
+        plt.figure()
+        plt.title("rate: "+str(rate)+", strength: "+str(strength))
+        plt.plot(spikes_it[1], spikes_it[0], ' .', mew=0)
 
         print 'rate:', rate, 'strength:', strength, REDO_FFTMAX
 plt.show()
