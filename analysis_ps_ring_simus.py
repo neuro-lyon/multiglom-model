c94f0d69392e6fd64efbecfb3009862921d52627
Vincent <vincent@ra.fake>
2013-06-18 16:57:59 +0200

    Fix bug that returned wrong interconnection rate

--- analysis_ps_ring_simus.py
+++ analysis_ps_ring_simus.py
@@ -3,6 +3,7 @@ import tables
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def main(dbfile):
     # Get the simulations
     db = tables.openFile(dbfile)
@@ -10,7 +11,7 @@ def main(dbfile):
 
     # Define some function to get specific result values
     def get_strength(simu):
-        return hm.get_group_attr(simu, ('paramset', '_v_attrs', 'Common', 'inter_conn_strength', 0, 0))
+        return hm.get_group_attr(simu, ('paramset', '_v_attrs', 'Common', 'inter_conn_strength', 0, 1))
 
     def get_mps(simu):
         return hm.get_group_attr(simu, ('results', '_v_attrs', 'MPS', 'whole'))
@@ -35,10 +36,7 @@ def main(dbfile):
     plt.plot(res_indexes[:, 0], res_indexes[:, 2], '.', label="STS (whole)")
     plt.plot(res_indexes[:, 0], res_indexes[:, 3], '.', label="FFTMAX (mean)")
     plt.legend()
-    plt.ion()
     plt.show()
-    plt.ioff()
-    
 
     # Get simulations to plot
     plot_simus = raw_input("Plot simulations #").split(' ')
@@ -46,7 +44,6 @@ def main(dbfile):
          for psimu in plot_simus:
              simu = simus[int(psimu)]
              hm.plot_simulation(simu)
-
 
     db.close()
 
