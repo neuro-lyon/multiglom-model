b56da156f57f8fb17f2f2cb14cf772948c740372
Vincent Llorens <vincent@ra.fake>
2013-08-16 17:45:01 +0200

    Add sharex to fig: temporal signals

--- src/fig_temporal_signals.py
+++ src/fig_temporal_signals.py
@@ -17,25 +17,25 @@ def main():
     # Get signals
     # INPUT
     input_sig = sim_results.input.read()[0]
-    plt.subplot(4, 1, 1)
+    xaxis = plt.subplot(4, 1, 1)
     plt.plot(time[:max_time], input_sig[:max_time])
     plt.ylabel('g_input ($S m^{-2}$)')
 
     # MEMB POT
     memb_pot = sim_results.single_memb_pot.read()
-    plt.subplot(4, 1, 2)
+    plt.subplot(4, 1, 2, sharex=xaxis)
     plt.plot(time[:max_time], memb_pot[:max_time])
     plt.ylabel('V (1 mitrale)')
 
     # S
     s_sig = sim_results.s_granule.read()[0]
-    plt.subplot(4, 1, 3)
+    plt.subplot(4, 1, 3, sharex=xaxis)
     plt.plot(time[:max_time], s_sig[:max_time])
     plt.ylabel('s (granule)')
 
     # S_SYN_SELF
     sss_sig = sim_results.s_syn_self.read()[0]
-    plt.subplot(4, 1, 4)
+    plt.subplot(4, 1, 4, sharex=xaxis)
     plt.plot(time[:max_time], sss_sig[:max_time])
     plt.ylabel('s_syn_self')
     plt.xlabel("Temps (s)")
